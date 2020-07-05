18377f70e6294f2e8e461085f3661cc08a225e14
Anas Nashif <anas.nashif@intel.com>
2020-07-05 10:14:07 -0400

    enhance req script

--- scripts/create_req.py
+++ scripts/create_req.py
@@ -2,9 +2,7 @@
 
 import csv
 import sys
-
-filename = sys.argv[1]
-print(filename)
+import argparse
 
 HEADER = """
 /**
@@ -18,33 +16,71 @@ FOOTER = """
 */
 """
 
-grouped = dict()
 
-with open(filename) as fp:
-        cr = csv.DictReader(fp)
-        for row in cr:
-            #print(row)
-            group = row['group']
-            if row['normative'] == "FALSE":
-                continue
-            if not grouped.get(group, None):
-                grouped[group] = []
+args = None
 
-            grouped[group].append({'id': "{}".format(row['uid']), 'req': row['text'], 'name': row['header']})
+def parse_csv(filename):
+    grouped = dict()
+    with open(filename) as fp:
+            cr = csv.DictReader(fp)
+            for row in cr:
+                group = row['group']
+                if row['normative'] == "FALSE":
+                    continue
+                if not grouped.get(group, None):
+                    grouped[group] = []
+
+                uid = row.get('uid')
+                text = row.get('text').rstrip()
+                name = row.get('header').rstrip()
+                grouped[group].append({'id': uid, 'req': text, 'name': name})
+    return grouped
+
+def write_dox(grouped, filename="requirements.dox"):
+    with open(filename, "w") as req:
+        req.write(HEADER)
+
+        for r in grouped.keys():
+            comp = grouped[r]
+            req.write("\n@section {}\n\n".format(r))
+            for c in comp:
+                req.write("@subsection {} {}: {}\n{}\n\n\n".format(c['id'], c['id'], c['name'], c['req']))
+
+        req.write(FOOTER)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+                description="Generate Requirement in various formats.")
 
-with open("requirements.dox", "w") as req:
-    req.write(HEADER)
+    parser.add_argument('-c', '--csv-file',
+            help="Requirements file in CSV format")
 
-    for r in grouped.keys():
-        comp = grouped[r]
-        req.write("\n@section {}\n\n".format(r))
-        for c in comp:
-            req.write("@subsection {} {}: {}\n{}\n\n\n".format(c['id'], c['id'], c['name'], c['req']))
+    parser.add_argument('-d', '--dox-file',
+            help="Output file in DOX format for Doxygen.")
 
-    req.write(FOOTER)
+    return parser.parse_args()
 
+def main():
+    global args
+    args = parse_args()
+
+    grouped = dict()
+    if args.csv_file:
+        print("Parsing CSV file...")
+        grouped = parse_csv(args.csv_file)
+
+    if grouped:
+        if args.dox_file is not None:
+            print(f"Writing DOX file {args.dox_file}...")
+            write_dox(grouped, args.dox_file)
+        else:
+            print("No output file given...")
+    else:
+        print("Nothing to do...")
+
+if __name__ == "__main__":
+    main()
 
 
 
