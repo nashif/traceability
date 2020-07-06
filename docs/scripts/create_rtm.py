78c4c41560e278800cbd5afff7029ea21b8c3834
Anas Nashif <anas.nashif@intel.com>
2020-07-06 09:24:39 -0400

    rtm: reformat code

--- docs/scripts/create_rtm.py
+++ docs/scripts/create_rtm.py
@@ -4,30 +4,20 @@ import argparse
 import os
 import sys
 
+from junitparser import TestCase, TestSuite, JUnitXml
 from lxml import objectify
-
-
 from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.chart import Reference, BarChart3D
+from openpyxl.styles import Alignment, Font
 from openpyxl.utils import get_column_letter
-from openpyxl.chart import (
-    PieChart,
-    ProjectedPieChart,
-    Reference
-)
-from openpyxl.chart.series import DataPoint
-from openpyxl.chart import BarChart, Series, Reference, BarChart3D
-
-from junitparser import TestCase, TestSuite, JUnitXml, Skipped, Error
-
 
 args = None
-
-DEBUG = False
+DEBUG = True
 
 def debug(str):
     if DEBUG:
         print(str)
+
 
 class TestCase:
     def __init__(self, name, refid=None, anchor=None):
@@ -39,6 +29,7 @@ class TestCase:
 
     def __str__(self):
         return self.name
+
 
 class TestSuite:
     def __init__(self, *args, **kwargs):
@@ -59,7 +50,6 @@ class TestSuite:
             if tc.refid == refid:
                 return tc
         return None
-
 
     def dump(self, group=None):
         for tc in self.testcases:
@@ -135,13 +125,16 @@ class TestSuite:
                     self.add(tc)
 
         debug("====== Done Parsing Tests ========")
+
+
 class Implementation:
     def __init__(self, name, refid=None, anchor=None, file=None, line=0):
         self.name = name
         self.refid = refid
         self.anchor = anchor
-        self.file=file
-        self.line=line
+        self.file = file
+        self.line = line
+
 
 class Requirement:
     def __init__(self, name, refid=None, anchor=None):
@@ -175,6 +168,7 @@ class Requirement:
             print("   - line: {}".format(i.line))
         print("\n")
 
+
 class Requirements:
     def __init__(self):
         self.requirements = []
@@ -189,7 +183,6 @@ class Requirements:
         r = Requirement(name=name)
         self.add(r)
         return r
-
 
     def tests(self):
         tests = set()
@@ -265,6 +258,7 @@ class Requirements:
             for subreq in req.xpath("//sect2"):
                 self._add_req(req_file, subreq, 2, name)
 
+
 class TestReport:
 
     def __init__(self, *args, **kwargs):
@@ -286,6 +280,7 @@ class TestReport:
 
             for testcase in suite:
                 self.testcases.append(testcase)
+
 
 class RTM():
     def __init__(self, xml_root, report_file=None):
@@ -313,10 +308,9 @@ class RTM():
         index = objectify.parse(index_file)
         groups = index.xpath('//compound[@kind="group"]')
         for group in groups:
-            refid= group.attrib['refid']
+            refid = group.attrib['refid']
             name = group.name
             self.groups[refid] = name
-
 
     def parse_xml(self):
         self.parse_index()
@@ -352,56 +346,66 @@ class RTM():
             print(str(e))
             sys.exit(2)
 
-        if False:
-            try:
-                satisfy_list = objectify.parse("{}/satisfy.xml".format(self.xml_root))
-                varlist = satisfy_list.xpath("//variablelist/node()")
-                for ve in varlist:
-                    refs = ve.xpath("term/ref")
-                    for ref in refs:
-                        refid = str(ref.xpath("@refid")[0])
-                        print(refid)
-                        group = refid.split("_ga", 1)[0]
-                        print(group)
-                        xmlid = refid.split("_1ga", 1)[-1]
-                        group_file = refid.split("_1ga", 1)[0]
+        try:
+            satisfy_list = objectify.parse("{}/satisfy.xml".format(self.xml_root))
+            varlist = satisfy_list.xpath("//variablelist/node()")
+            for ve in varlist:
+                refs = ve.xpath("term/ref")
+                for ref in refs:
+                    refid = str(ref.xpath("@refid")[0])
+                    debug(refid)
+                    group = refid.split("_ga", 1)[0]
+                    debug(group)
+                    xmlid = refid.split("_1ga", 1)[-1]
+                    debug(xmlid)
+                    group_file = refid.split("_1ga", 1)[0]
+                    if xmlid not in group:
                         func_id = "{}_1ga{}".format(group, xmlid)
-                        if os.path.exists(os.path.join(self.xml_root, f"{group_file}.xml")):
-                            xmlfile = "{}.xml".format(group_file)
-                        else:
-                            xmlfile = "{}.xml".format(group)
-                        impl = objectify.parse(os.path.join(self.xml_root, xmlfile))
-                        file = impl.xpath("//*[@id='{}']//location/@file".format(func_id))[0]
+                    else:
+                        func_id = group
+
+                    if os.path.exists(os.path.join(self.xml_root, f"{group_file}.xml")):
+                        xmlfile = "{}.xml".format(group_file)
+                    else:
+                        xmlfile = "{}.xml".format(group)
+                    debug(xmlfile)
+                    debug(func_id)
+                    impl = objectify.parse(os.path.join(self.xml_root, xmlfile))
+
+                    file_list = impl.xpath("//*[@id='{}']//location/@file".format(func_id))
+                    if file_list:
+                        file = file_list[0]
                         line = impl.xpath("//*[@id='{}']//location/@line".format(func_id))[0]
-                        impl = Implementation(
-                            name=str(ref),
-                            refid=refid,
-                            file=file,
-                            line=line
-                        )
 
-                    listitems = ve.xpath("para/ref")
-                    for item in listitems:
-                        requirement = str(item)
-                        r = self.requirements.get_or_create(requirement)
-                        r.add_implementation(impl)
+                    impl = Implementation(
+                        name=str(ref),
+                        refid=refid,
+                        file=file,
+                        line=line
+                    )
 
-            except OSError as e:
-                print(str(e))
-                sys.exit(2)
+                listitems = ve.xpath("para/ref")
+                for item in listitems:
+                    requirement = str(item)
+                    r = self.requirements.get_or_create(requirement)
+                    r.add_implementation(impl)
+
+        except OSError as e:
+            print(str(e))
+            sys.exit(2)
 
     @staticmethod
     def adjust_width(worksheet):
         wrap = False
         for col in worksheet.columns:
             max_length = 0
-            column = col[0].column # Get the column name
+            column = col[0].column  # Get the column name
             for cell in col:
                 if cell.alignment.wrapText:
                     wrap = True
-                if cell.coordinate in worksheet.merged_cells: # not check merge_cells
+                if cell.coordinate in worksheet.merged_cells:  # not check merge_cells
                     continue
-                try: # Necessary to avoid error on empty cells
+                try:  # Necessary to avoid error on empty cells
                     if len(str(cell.value)) > max_length:
                         max_length = len(cell.value)
                 except:
@@ -411,8 +415,6 @@ class RTM():
                 continue
             adjusted_width = (max_length + 2) * 1.2
             worksheet.column_dimensions[get_column_letter(column)].width = adjusted_width
-
-
 
     def write_srs(self, filename):
         SRS_HEADER = """
@@ -435,13 +437,10 @@ Software capabilities
 
                     for c in self.requirements.get_by_parent(r.name):
                         req.write(f".. item:: {c.name} {c.title}\n")
-                        #req.write("\n")
+                        # req.write("\n")
                         for t in c.tests:
                             req.write(f"    :validated_by: {t.name}\n")
                         req.write(f"\n    {c.text.rstrip()}\n\n")
-
-
-
 
     def write_rst(self, output_dir="docs"):
 
@@ -455,10 +454,9 @@ Software capabilities
             fp.write(f"{section}\n{len(section) * '='}\n\n")
             for item in self.suite.testcases:
                 fp.write(f".. item:: {item.name} {item.brief}\n")
-                #for v in self.requirements.get_by_test(item.name):
+                # for v in self.requirements.get_by_test(item.name):
                 #    fp.write(f"    :validates: {v}\n")
                 fp.write("\n")
-
 
     def write_xls(self, output_file):
         book = Workbook()
@@ -467,7 +465,7 @@ Software capabilities
         testplan_sheet = book.create_sheet("Tests")
         sheet3 = book.create_sheet("Requirements")
 
-        #sheet1.row(0).height_mismatch = True
+        # sheet1.row(0).height_mismatch = True
         sheet1.row_dimensions[1].height = 70
 
         # Write Testplan
@@ -478,7 +476,6 @@ Software capabilities
         cell = testplan_sheet.cell(1, 1, "ID")
         cell.font = Font(bold=True)
         header_alignment = Alignment(horizontal='center', vertical='top')
-
 
         cell = testplan_sheet.cell(1, 2, "Brief")
         cell.alignment = header_alignment
@@ -496,7 +493,6 @@ Software capabilities
                 testplan_sheet.cell(row, 2, tc.brief)
             test_links[tc.name] = 'A{}'.format(row)
             row = row + 1
-
 
         # Write Requirements sheet
         sheet3.column_dimensions['A'].width = 50
@@ -516,7 +512,6 @@ Software capabilities
         cell.alignment = header_alignment
         cell.font = Font(bold=True)
 
-
         row = 2
         links = {}
         for r in self.requirements.requirements:
@@ -532,24 +527,25 @@ Software capabilities
         # Now go through all tests
         for t in self.suite.testcases:
             cell = sheet1.cell(row, 1, '=HYPERLINK("#Tests!{}","{}")'.format(test_links[t.name], t.name))
-            #cell.font = Font(bold=True)
-            #header_alignment = Alignment(horizontal='center', vertical='top')
-            #cell.alignment = header_alignment
+            # cell.font = Font(bold=True)
+            # header_alignment = Alignment(horizontal='center', vertical='top')
+            # cell.alignment = header_alignment
 
             sheet1.column_dimensions['A'].width = 50
             # Get all requirements for a test
             for r in self.requirements.requirement(t.name):
                 if r not in req_cols:
                     debug(f"{1}x{len(req_cols) + 2}")
-                    cell = sheet1.cell(1, len(req_cols) + 3,'=HYPERLINK("#Requirements!{}","{}")'.format(links[r], r))
-                    header_alignment1 = Alignment(horizontal='center', vertical='top', textRotation=90, shrinkToFit=True)
+                    cell = sheet1.cell(1, len(req_cols) + 3, '=HYPERLINK("#Requirements!{}","{}")'.format(links[r], r))
+                    header_alignment1 = Alignment(horizontal='center', vertical='top', textRotation=90,
+                                                  shrinkToFit=True)
                     cell.font = Font(bold=True)
                     cell.alignment = header_alignment1
 
                     col = get_column_letter(cell.column)
                     sheet1.column_dimensions[col].width = 3
 
-                    #sheet1.col(len(req_cols) + 2).width = 256 * 4
+                    # sheet1.col(len(req_cols) + 2).width = 256 * 4
                     req_cols[r] = len(req_cols) + 3
 
                 sheet1.cell(row, req_cols[r], "X")
@@ -559,7 +555,8 @@ Software capabilities
         for r in self.requirements.requirements:
             if r.name not in req_cols:
                 debug(f"{1}x{len(req_cols) + 2}")
-                cell = sheet1.cell(1, len(req_cols) + 3,'=HYPERLINK("#Requirements!{}","{}")'.format(links[r.name], r.name))
+                cell = sheet1.cell(1, len(req_cols) + 3,
+                                   '=HYPERLINK("#Requirements!{}","{}")'.format(links[r.name], r.name))
                 header_alignment = Alignment(horizontal='center', vertical='top', textRotation=90, shrinkToFit=True)
                 cell.alignment = header_alignment
                 cell.font = Font(bold=True)
@@ -576,15 +573,17 @@ Software capabilities
         row1 = 3
         tests = self.suite.testcases
         for t in tests:
-            cell= "=COUNTA({}{}:{}{})".format(get_column_letter(3), row1, get_column_letter(1 + len(self.requirements.requirements)), row1 )
+            cell = "=COUNTA({}{}:{}{})".format(get_column_letter(3), row1,
+                                               get_column_letter(1 + len(self.requirements.requirements)), row1)
             sheet1.cell(row1, 2, cell)
             row1 = row1 + 1
 
-        sheet1.cell(2, 2, "=SUM(B3:B{})".format(3+len(tests)))
+        sheet1.cell(2, 2, "=SUM(B3:B{})".format(3 + len(tests)))
 
         col = 3
         for t in self.requirements.requirements:
-            sheet1.cell(2, col, "=COUNTA({}3:{}{})".format(get_column_letter(col), get_column_letter(col), 3 + len(tests)))
+            sheet1.cell(2, col,
+                        "=COUNTA({}3:{}{})".format(get_column_letter(col), get_column_letter(col), 3 + len(tests)))
             col = col + 1
 
         # Report
@@ -597,7 +596,6 @@ Software capabilities
             cell.font = Font(bold=True)
             header_alignment = Alignment(horizontal='center', vertical='top')
             cell.alignment = header_alignment
-
 
             r = TestReport()
             r.parse(self.report_file)
@@ -617,10 +615,11 @@ Software capabilities
 
                 # Targets (Header)
                 if target not in targets:
-                    for c in [1,11]:
+                    for c in [1, 11]:
                         cell = test_report.cell(c, tidx, target)
                         cell.font = Font(bold=True)
-                        cell.alignment = Alignment(horizontal='center', vertical='top', textRotation=90, shrinkToFit=True)
+                        cell.alignment = Alignment(horizontal='center', vertical='top', textRotation=90,
+                                                   shrinkToFit=True)
                     targets.append(target)
                     test_report.column_dimensions[get_column_letter(tidx)].width = 8
                     tidx += 1
@@ -643,7 +642,6 @@ Software capabilities
                 results[t] = 'A{}'.format(row + 1)
                 row = row + 1
 
-
             row = 2
             summary = ["pass", "failure", "skipped", "error", "total", "passrate", "execution"]
             for item in summary:
@@ -660,15 +658,20 @@ Software capabilities
                         formula = "=({col}2+{col}3+{col}5)/{col}6".format(col=get_column_letter(col))
                         style = "Percent"
                     elif item == "skipped":
-                        formula = '=COUNTIF({}$12:{}${},"{}") + COUNTBLANK({}$12:{}${})'.format(get_column_letter(col),get_column_letter(col), len(tt), item, get_column_letter(col), get_column_letter(col), len(tt))
+                        formula = '=COUNTIF({}$12:{}${},"{}") + COUNTBLANK({}$12:{}${})'.format(get_column_letter(col),
+                                                                                                get_column_letter(col),
+                                                                                                len(tt), item,
+                                                                                                get_column_letter(col),
+                                                                                                get_column_letter(col),
+                                                                                                len(tt))
                     else:
-                        formula = '=COUNTIF({}$12:{}${},"{}")'.format(get_column_letter(col),get_column_letter(col), len(tt), item)
+                        formula = '=COUNTIF({}$12:{}${},"{}")'.format(get_column_letter(col), get_column_letter(col),
+                                                                      len(tt), item)
 
                     cell = test_report.cell(row, col, formula)
                     cell.style = style
                     col += 1
                 row += 1
-
 
             chart1 = BarChart3D()
             chart1.style = 11
@@ -684,8 +687,7 @@ Software capabilities
             chart1.width = 25
             chart1.height = 15
 
-
-            #chart1.shape = 4
+            # chart1.shape = 4
             test_report.add_chart(chart1, "{}10".format(get_column_letter(len(targets) + 5)))
 
         self.adjust_width(testplan_sheet)
@@ -694,44 +696,43 @@ Software capabilities
         book.save(output_file)
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser(
-                description="Generate Requirement Traceability Matrix (RTM).")
+        description="Generate Requirement Traceability Matrix (RTM).")
 
     parser.add_argument('-x', '--xmlroot', default=None,
-            help="Root directory of XML files generated by doxygen.")
+                        help="Root directory of XML files generated by doxygen.")
 
     parser.add_argument('-r', '--rtm-file',
-            help="RTM file in Excel format")
+                        help="RTM file in Excel format")
 
     parser.add_argument('--generate-rst', action="store_true",
-            help="Generate RST files")
+                        help="Generate RST files")
 
     parser.add_argument('--output-dir',
-            help="Output directory")
+                        help="Output directory")
 
     parser.add_argument('-g', '--group',
-            help="filter by group")
+                        help="filter by group")
 
     parser.add_argument('-p', '--test-prefix', default="test_",
-            help="Testcase prefix to identify testcases")
+                        help="Testcase prefix to identify testcases")
 
     parser.add_argument('-i', '--add-test-identifiers', action="store_true",
-            help="Add additional identifiers based on test groups")
+                        help="Add additional identifiers based on test groups")
 
     parser.add_argument('-d', '--dump', action="store_true",
-    help="Dump requirements and dependencies")
+                        help="Dump requirements and dependencies")
 
     parser.add_argument("-j", "--junit-file", help="file with test results in junit format")
     return parser.parse_args()
+
 
 def main():
     global args
 
     args = parse_args()
     if not args.xmlroot:
-
         sys.exit("No XML root given")
 
     if args.junit_file and not os.path.exists(args.junit_file):
@@ -761,7 +762,6 @@ def main():
         print("Generating RST files...")
         rtm.write_rst(args.output_dir)
 
+
 if __name__ == "__main__":
     main()
-
-
