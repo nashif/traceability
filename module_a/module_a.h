f941fcbcc985fd74a248f238e08d202ab4374e0d
Anas Nashif <anas.nashif@intel.com>
2020-07-06 09:24:15 -0400

    module_a: fix satisfy links

--- module_a/module_a.h
+++ module_a/module_a.h
@@ -16,7 +16,8 @@
  * @param b byte two
  * @param c byte three
  * @return int8_t The average of all bytes
- * @satisfy{@req{001}}
+ * @satisfy{@req{002}}
+ * @satisfy{@req{003}}
  */
 int8_t average_three_bytes(int8_t a, int8_t b, int8_t c);
 
